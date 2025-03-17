f82fe76c6a602e6cb1d40b14bce198274077032f
procras <procras2@gmail.com>
2025-03-17 15:22:02 +0000

    i3

---
+++ stowit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script to restore configuration files from repos
+# procras
+
+stow -t ~ backgrounds
+stow -t ~ bashrc
+stow -t ~ dircolors
+stow -t ~ i3
+stow -t ~ polybar
+stow -t ~ profile
+stow -t ~ xinitrc
+stow -t ~ xprofile
+stow -t ~ Xresources
+stow -t ~ zathura
