84f6f54cbe82bf75ef7a005eeac872a1f57ef90b
Adrian P. Ireland <procras2@gmail.com>
2025-03-21 09:30:49 +0000

    dpms improvements

--- bin/bin/lockscreen.sh
+++ bin/bin/lockscreen.sh
@@ -10,6 +10,18 @@ trap "dunstctl set-paused false" EXIT
 # pause notifications
 dunstctl set-paused true
 
-# enable the lockscreen
-i3lock --nofork -c 272d2d -i /home/adrian/.config/backgrounds/nice-blue-background.png
 
+# https://www.reddit.com/r/i3wm/comments/9089o2/turn_off_screen_after_a_moment_if_locked/?rdt=63250
+
+img=/tmp/i3lock.png
+scrot -o $img
+convert $img -scale 10% -scale 1000% $img
+
+# enable the lockscreen
+i3lock --nofork -c 272d2d -i $img &
+
+while [ $(pgrep i3lock | wc -l ) -ne 0 ];
+do
+    sleep 420
+    xset dpms force off
+done
