939f16e82f47ec702e5ede687d558e54434666eb
Adrian P. Ireland <procras2@gmail.com>
2026-07-06 07:34:20 +0100

    Dictionary and Thesaurus word completions

--- nvim-artorias305/.config/nvim/README.md
+++ nvim-artorias305/.config/nvim/README.md
@@ -11,3 +11,5 @@ Minimal neovim config for 2026 neovim 0.12 up
 By Artorias305
 
 - [GitHub](https://github.com/artorias305/nvim/blob/14fe8f869393a957718d46b4a62169df8e4e307f/init.lua)
+
+If only I could get the Dictionary to work
--- nvim-artorias305/.config/nvim/lua/plugins.lua
+++ nvim-artorias305/.config/nvim/lua/plugins.lua
@@ -8,6 +8,8 @@ vim.pack.add({
     { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim"}, -- automatically enable servers installed by mason
     { src = "https://github.com/L3MON4D3/LuaSnip" },
     { src = "https://github.com/rafamadriz/friendly-snippets" },
+    -- { src = "https://github.com/Kaiser-Yang/blink-cmp-dictionary"},
+    { src = "https://github.com/archie-judd/blink-cmp-words"},
     { src = "https://github.com/saghen/blink.cmp", version = 'v1.8.0' },
     { src = "https://github.com/nvim-mini/mini.nvim" },
     -- {
@@ -109,7 +111,81 @@ require("blink.cmp").setup({
 			},
 		},
 	},
+    sources = {
+        default = { "lsp", "path", "buffer", "snippets" },
+        providers = {
+
+            -- Use the thesaurus source
+            thesaurus = {
+                name = "blink-cmp-words",
+                module = "blink-cmp-words.thesaurus",
+                -- All available options
+                opts = {
+                    -- A score offset applied to returned items. 
+                    -- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+                    score_offset = 0,
+
+                    -- Default pointers define the lexical relations listed under each definition,
+                    -- see Pointer Symbols below.
+                    -- Default is as below ("antonyms", "similar to" and "also see").
+                    definition_pointers = { "!", "&", "^" },
+
+                    -- The pointers that are considered similar words when using the thesaurus,
+                    -- see Pointer Symbols below.
+                    -- Default is as below ("similar to", "also see" }
+                    similarity_pointers = { "&", "^" },
+
+                    -- The depth of similar words to recurse when collecting synonyms. 1 is similar words, 
+                    -- 2 is similar words of similar words, etc. Increasing this may slow results. 
+                    similarity_depth = 2,
+                },
+            },
+
+            -- Use the dictionary source
+            dictionary = {
+                name = "blink-cmp-words",
+                module = "blink-cmp-words.dictionary",
+                -- All available options
+                opts = {
+                    -- The number of characters required to trigger completion. 
+                    -- Set this higher if completion is slow, 3 is default.
+                    dictionary_search_threshold = 3,
+
+                    -- See above
+                    score_offset = 0,
+
+                    -- See above
+                    definition_pointers = { "!", "&", "^" },
+                },
+            },
+        },
+
+        -- Setup completion by filetype
+        per_filetype = {
+            text = { "dictionary" },
+            markdown = { "thesaurus" },
+        },
+    },
 })
+    -- sources = {
+    --     default = { "dictionary", "lsp", "path", "buffer", "snippets" },
+    --     providers = {
+    --         dictionary = {
+    --             module = 'blink-cmp-dictionary',
+    --             name = 'Dict',
+    --             min_keyword_length = 1,
+    --             opts = {
+    --                 dictionary_files = nil,
+    --                 dictionary_directories = nil,
+    --                 -- dictionary_files = { vim.fn.expand('~/.config/nvim/dictionary') }
+    --                 -- Optional: explicitly force fallback mode
+    --                 -- (By default, fallback is used when fzf is not found)
+    --                 force_fallback = true
+    --             }
+    --         },
+    --     }
+    -- }
+-- })
 
 -- Mini
 require("mini.icons").setup()
