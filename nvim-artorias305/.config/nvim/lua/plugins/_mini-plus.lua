73f48cc94f7e5ecb34088d3f9cae1c1192ccc609
Adrian P. Ireland <procras2@gmail.com>
2026-07-09 15:37:58 +0100

    Mini pick <C-q> list to quickfix

--- nvim-artorias305/.config/nvim/lua/plugins/_mini-plus.lua
+++ nvim-artorias305/.config/nvim/lua/plugins/_mini-plus.lua
@@ -11,3 +11,13 @@ vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
 vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
 vim.keymap.set("n", "<leader>g", ":Pick grep_live<CR>")
 vim.keymap.set("n", "<leader>b", ":Pick buffers<CR>")
+
+local choose_all = function()
+	local mappings = MiniPick.get_picker_opts().mappings
+	vim.api.nvim_input(mappings.mark_all .. mappings.choose_marked)
+end
+require("mini.pick").setup({
+	mappings = {
+		choose_all = { char = "<C-q>", func = choose_all },
+	},
+})
